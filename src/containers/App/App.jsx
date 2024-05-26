bb913965bc73d9d91f39280a5824289a7de61c5d
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-05-26 18:16:51 +0500

    10 урок, установка и использование react-router

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,29 @@
-import styles from './App.module.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
-import PeoplePage from '@containers/PeoplePage';
+import routesConfig from '@routes/routesConfig';
+import Header from '@components/Header';
+
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <PeoplePage />
+    <>
+      <BrowserRouter>
+        <div className={styles.wrapper}>
+          <Header />
+          <Routes>
+            {routesConfig.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/routes/routesConfig.js
@@ -0,0 +1,17 @@
+import HomePage from '@containers/HomePage';
+import PeoplePage from '@containers/PeoplePage';
+
+const routesConfig = [
+    {
+        path: '/',
+        exact: true,
+        element: <HomePage />
+    },
+    {
+        path: '/people',
+        exact: true,
+        element: <PeoplePage />
+    }
+]
+
+export default routesConfig;
