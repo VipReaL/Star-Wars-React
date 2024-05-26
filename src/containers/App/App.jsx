283518e7b852025f18711222bd4e398707dcf646
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-05-26 15:40:55 +0500

    8 урок - установка и настройка Alias

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,6 @@
 import styles from './App.module.css';
 
-import PeoplePage from '../PeoplePage/PeoplePage';
+import PeoplePage from '@containers/PeoplePage';
 
 const App = () => {
   return (
--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -2,11 +2,11 @@ import styles from './PeoplePage.module.css';
 
 import { useState, useEffect } from 'react';
 
-import { getApiResource } from '../../utils/network';
-import { API_PEOPLE } from '../../constants/api';
-import { getPeopleId, getPeopleImage } from '../../services/getPeopleData';
-import { withErrorApi } from '../../hoc-helpers/withErrorApi';
-import PeopleList from '../../components/PeoplePage/PeopleList';
+import { getApiResource } from '@utils/network';
+import { API_PEOPLE } from '@constants/api';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
+import { withErrorApi } from '@hoc-helpers/withErrorApi';
+import PeopleList from '@components/PeoplePage/PeopleList';
 
 const PeoplePage = ({ setErrorApi }) => {
   const [people, setPeople] = useState(null);
