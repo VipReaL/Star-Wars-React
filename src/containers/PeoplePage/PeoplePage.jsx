77058eb3f9bde0a67640e722a152bf9a752c9a26
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-05-26 16:28:15 +0500

    9 урок, установка и использование prop-types

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,12 +1,13 @@
-import styles from './PeoplePage.module.css';
-
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import { getApiResource } from '@utils/network';
-import { API_PEOPLE } from '@constants/api';
-import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
 import { withErrorApi } from '@hoc-helpers/withErrorApi';
 import PeopleList from '@components/PeoplePage/PeopleList';
+import { getApiResource } from '@utils/network';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
+import { API_PEOPLE } from '@constants/api';
+
+import styles from './PeoplePage.module.css';
 
 const PeoplePage = ({ setErrorApi }) => {
   const [people, setPeople] = useState(null);
@@ -43,4 +44,8 @@ const PeoplePage = ({ setErrorApi }) => {
   )
 }
 
-  export default withErrorApi(PeoplePage);
+PeoplePage.propTypes = {
+  setErrorApi: PropTypes.func
+}
+
+export default withErrorApi(PeoplePage);
