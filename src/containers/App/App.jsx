664e2d112d7880974c451f3d9753cdd17d1918a2
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-08-08 20:04:56 +0500

    commit

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,13 +2,14 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import routesConfig from '@routes/routesConfig';
 import Header from '@components/Header';
+import { REPO_NAME } from '@constants/repo'
 
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <div className={styles.wrapper}>
           <Header />
           <Routes>
